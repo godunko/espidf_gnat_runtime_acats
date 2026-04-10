ae391f69c79fa36629f0a7278c143ad0aca9c7ed
Vadim Godunko <vgodunko@gmail.com>
2026-04-10 13:54:35 +0400

    Class C, Appex F

--- bundle_1/source/main.adb
+++ bundle_1/source/main.adb
@@ -74,6 +74,27 @@ with AD7006A;
 --  with AE3702A;
 --  with AE3709A;
 
+with CXF1001;
+with CXF2001;
+with CXF2002;
+with CXF2003;
+with CXF2004;
+with CXF2005;
+with CXF2A01;
+with CXF2A02;
+--  with CXF3001;
+--  with CXF3002;
+--  with CXF3003;
+--  with CXF3004;
+--  with CXF3A01;
+--  with CXF3A02;
+--  with CXF3A03;
+--  with CXF3A04;
+--  with CXF3A05;
+--  with CXF3A06;
+--  with CXF3A07;
+--  with CXF3A08;
+
 procedure Main is
    procedure ESP_Restart
      with Import, Convention => C, External_Name => "esp_restart";
@@ -154,6 +175,27 @@ begin
    --  AE3101A;
    --  AE3702A;
    --  AE3709A;
+
+   CXF1001;
+   CXF2001;
+   CXF2002;
+   CXF2003;
+   CXF2004;
+   CXF2005;
+   CXF2A01;
+   CXF2A02;
+   --  CXF3001;
+   --  CXF3002;
+   --  CXF3003;
+   --  CXF3004;
+   --  CXF3A01;
+   --  CXF3A02;
+   --  CXF3A03;
+   --  CXF3A04;
+   --  CXF3A05;
+   --  CXF3A06;
+   --  CXF3A07;
+   --  CXF3A08;
 
    ESP_Restart;
 end Main;
